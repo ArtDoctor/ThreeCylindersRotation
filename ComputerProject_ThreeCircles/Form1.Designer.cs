7b4577a8506d7a067a06efa8d61551635418368d
Ash <46786959+ArtDoctor@users.noreply.github.com>
2022-06-24 12:26:47 +0200

    Add files via upload

---
+++ ComputerProject_ThreeCircles/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace ComputerProject_ThreeCircles
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "R1 =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "R2 =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "R3 =";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 31);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 31);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "5";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(59, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(114, 31);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "7";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(59, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(114, 31);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "w =";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Result:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(265, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(479, 288);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 267);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Scale(px:m):";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(178, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "(m)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(178, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 24);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "(m)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(178, 117);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 24);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "(m)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(178, 165);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 24);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "(rad/s)";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(750, 11);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(253, 336);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 356);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "ThreeCircles";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ComputerProject_ThreeCircles/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerProject_ThreeCircles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        bool ka = false;
+        double R1, R2, R3, center;
+        double a, b, c;
+
+
+        double k, w1, w2, w3, v;
+        Graphics g;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            R1 = double.Parse(textBox1.Text);
+            R2 = double.Parse(textBox2.Text);
+            R3 = double.Parse(textBox3.Text);
+            w2 = double.Parse(textBox4.Text);
+            a = 0; b = 0; c = 0;
+            v = w2 * R2;
+            w3 = -v / R3;
+            w1 = -v / R1;
+            label5.Text = "Result(m/s): w1 =" + Math.Round(w1, 2) + ", w2 = " + Math.Round(w2, 2) + ", w3 = " + Math.Round(w3, 2);
+
+            //Scale
+            double L = pictureBox1.Width - 50;
+            double l = 2 * (R1 + R2 + R3);
+            k = l / L;
+            R1 = (int)(R1 / k);
+            R2 = (int)(R2 / k);
+            R3 = (int)(R3 / k);
+            label6.Text = "Scale(px:m): 1:" + Math.Round(k, 2);
+
+            //Drawing circles
+            center = pictureBox1.Height / 2;
+            g = pictureBox1.CreateGraphics();
+            g.DrawEllipse(Pens.Black, 25, (int)(center - R1), (int)(R1 * 2), (int)(R1 * 2));
+            g.DrawEllipse(Pens.Black, (int)(25 + R1 * 2), (int)(center - R2), (int)(R2 * 2), (int)(R2 * 2));
+            g.DrawEllipse(Pens.Black, (int)(25 + R1 * 2 + R2 * 2), (int)(center - R3), (int)(R3 * 2), (int)(R3 * 2));
+
+            ka = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (ka)
+            {
+                g.DrawLine(Pens.White, (int)(25 + R1), (int)center, (int)(25 + R1 + (R1 - 2) * Math.Cos(a)), (int)(center + (R1 - 2) * Math.Sin(a)));
+                g.DrawLine(Pens.White, (int)(25 + R2 + 2 * R1), (int)center, (int)(25 + R1 * 2 + R2 + (R2 - 2) * Math.Cos(b)), (int)(center + (R2 - 2) * Math.Sin(b)));
+                g.DrawLine(Pens.White, (int)(25 + R3 + 2 * R2 + 2 * R1), (int)center, (int)(25 + R3 + R1 * 2 + R2 * 2 + (R3 - 2) * Math.Cos(c)), (int)(center + (R3 - 2) * Math.Sin(c)));
+                a += w1 / 100;
+                b += w2 / 100;
+                c += w3 / 100;
+                g.DrawLine(Pens.Black, (int)(25 + R1), (int)center, (int)(25 + R1 + (R1 - 2) * Math.Cos(a)), (int)(center + (R1 - 2) * Math.Sin(a)));
+                g.DrawLine(Pens.Black, (int)(25 + R2 + 2 * R1), (int)center, (int)(25 + R1 * 2 + R2 + (R2 - 2) * Math.Cos(b)), (int)(center + (R2 - 2) * Math.Sin(b)));
+                g.DrawLine(Pens.Black, (int)(25 + R3 + 2 * R2 + 2 * R1), (int)center, (int)(25 + R3 + R1 * 2 + R2 * 2 + (R3 - 2) * Math.Cos(c)), (int)(center + (R3 - 2) * Math.Sin(c)));
+            }
+        }
+    }
+}
